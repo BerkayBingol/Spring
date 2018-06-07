7bed380e84dbabe633b2576d2fda30dccbc46af7
BerkayBingol <berkaybingol35@gmail.com>
2018-06-07 18:46:24 +0300

    basic2

---
+++ src/com/HelloSpring/HelloSpring.java
@@ -0,0 +1,15 @@
+package com.HelloSpring;
+ 
+class HelloSpring {
+ 
+ private String message;
+ 
+ public void setMessage(String message) {
+ this.message = message;
+ }
+ 
+ public void getMessage() {
+ System.out.println("Your Message : " + message);
+ }
+ 
+}
